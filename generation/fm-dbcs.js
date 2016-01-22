c3122368d76f8c75d787034d530543f0b2f7abca
Yonggang Luo <luoyonggang@gmail.com>
2016-01-23 02:24:40 +0800

    JIS related encodings should be handled independently.

--- generation/fm-dbcs.js
+++ generation/fm-dbcs.js
@@ -56,21 +56,13 @@ async.parallel({
 }, errTo(console.log, function (data) {
   // First, parse all files.
   for (let enc in data) {
-    let isJIS = enc.startsWith('$jis')
     let dbcs = []
-    if (isJIS) {
-      dbcs = {}
-    }
     let parseIntBase = enc.startsWith('$') ? 10 : 16
     utils.parseText(data[enc]).map(function (a) {
       let dbcsCode = parseInt(a[0], parseIntBase)
       let unicode = parseInt(a[1], 16)
       if (!isNaN(unicode)) {
-        if (isJIS) {
-          dbcs[dbcsCode] = unicode
-        } else {
-          dbcs.push([dbcsCode, unicode])
-        }
+        dbcs.push([dbcsCode, unicode])
       }
     })
     data[enc] = dbcs
@@ -106,51 +98,8 @@ async.parallel({
   }
   utils.writeFile('gb18030-ranges', JSON.stringify(ranges))
 
-  // Use http://encoding.spec.whatwg.org/#shift_jis-decoder
-  let shiftjis = initDBCS(0x80)
-  for (let i = 0xA1; i <= 0xDF; i++) {
-    shiftjis.push([i, 0xFF61 + i - 0xA1])
-  }
-
-  for (let lead = 0x81; lead < 0xFF; lead++) {
-    if (lead < 0xA1 || lead > 0xDF) {
-      for (let byte = 0; byte < 0xFF; byte++) {
-        let offset = (byte < 0x7F) ? 0x40 : 0x41
-        let leadOffset = (lead < 0xA0) ? 0x81 : 0xC1
-        if ((byte >= 0x40 && byte <= 0x7E) || (byte >= 0x80 && byte <= 0xFC)) {
-          let pointer = (lead - leadOffset) * 188 + byte - offset
-          if (data.$jis0208[pointer]) {
-            shiftjis.push([(lead << 8) + byte, data.$jis0208[pointer]])
-          } else if (pointer >= 8836 && pointer <= 10528) {
-            shiftjis.push([(lead << 8) + byte, 0xE000 + pointer - 8836]) // Interoperable legacy from Windows known as EUDC
-          }
-        }
-      }
-    }
-  }
-
-  utils.writeTable('shiftjis', utils.generateTable(shiftjis))
-
-  // Fill out EUC-JP table according to http://encoding.spec.whatwg.org/#euc-jp
-  let eucJp = initDBCS()
-  for (let i = 0xA1; i <= 0xDF; i++) {
-    eucJp.push([(0x8E << 8) + i, 0xFF61 + i - 0xA1])
-  }
-  for (let i = 0xA1; i <= 0xFE; i++) {
-    for (let j = 0xA1; j <= 0xFE; j++) {
-      let idx = (i - 0xA1) * 94 + (j - 0xA1)
-      const jis0208Char = data.$jis0208[idx]
-      const jis0212Char = data.$jis0212[idx]
-      // NOTE: We need to make sure the idx are exists
-      if (jis0208Char && jis0212Char) {
-        // console.log('Exist in both index')
-      }
-      if (jis0208Char) eucJp.push([(i << 8) + j, jis0208Char])
-      if (jis0212Char) eucJp.push([(0x8F << 16) + (i << 8) + j, jis0212Char])
-    }
-  }
-
-  utils.writeTable('eucjp', utils.generateTable(eucJp))
+  utils.writeTable('jis0208', utils.generateTable(data.$jis0208))
+  utils.writeTable('jis0212', utils.generateTable(data.$jis0212))
 
   // Fill out EUC-KR Table and check that it is the same as cp949.
   let eucKr = initDBCS()
